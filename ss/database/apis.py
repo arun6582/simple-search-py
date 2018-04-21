9d701137cfafdeee4fb9f791d6a1118fd6a8d390
mac-air-17 <arun6582@gmail.com>
2018-04-21 18:02:38 +0530

    skelton

---
+++ ss/database/apis.py
@@ -0,0 +1,9 @@
+class Index(object):
+
+    @classmethod
+    def save(self, data):
+        pass
+
+    @classmethod
+    def retrieve(self):
+        pass
---
+++ ss/search/apis.py
@@ -0,0 +1,9 @@
+class Index(object):
+
+    @classmethod
+    def create_index(self, data):
+        pass
+
+    @classmethod
+    def search(cls, query):
+        pass
