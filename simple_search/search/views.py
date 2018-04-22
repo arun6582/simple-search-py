23fc90d464f1b531b3e89741d96248b8cfb17306
mac-air-17 <arun6582@gmail.com>
2018-04-22 15:48:40 +0530

    templates

---
+++ simple_search/search/views.py
@@ -0,0 +1,24 @@
+from . import apis
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html', {})
+
+
+def index(request):
+    apis.Index.index(dict(request.POST))
+    return JsonResponse({'acknowledged': True})
+
+
+def search(request):
+    fields = request.GET.getlist('fields')
+    query = apis.Index.search(request.GET['query'])
+    response = apis.Index.search(
+        {
+            'fields': fields or ['_all', ],
+            'terms': query
+        }
+    )
+    return JsonResponse(response)
